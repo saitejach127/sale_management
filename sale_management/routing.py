89f4e7ec4e5db3e208b22d09a8e69cf93b647736
saitejach127 <saitejach127@gmail.com>
2023-02-04 18:20:41 +0530

    Removed unused routing

--- sale_management/routing.py
+++
@@ -1,8 +0,0 @@
-import frappe
-
-def get_home_page(user):
-    print("user is", user)
-    userdoc = frappe.get_doc(doctype ="User", filter={'email': user})
-    print(userdoc.roles, "user doc")
-    print("something")
-    return "login"
