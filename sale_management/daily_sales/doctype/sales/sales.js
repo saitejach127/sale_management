73c996dec12819221785d4072a43ad1e2f8d01d9
saitejach127 <saitejach127@gmail.com>
2023-02-24 19:41:21 +0000

    Sales Form modified

--- sale_management/daily_sales/doctype/sales/sales.js
+++ sale_management/daily_sales/doctype/sales/sales.js
@@ -1,8 +1,18 @@
 // Copyright (c) 2023, metamenu and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Sales", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Sales", {
+	refresh(frm) {
+        frappe.call({
+            method: 'sale_management.daily_sales.doctype.sales.sales.getUserCateringSite',
+            freeze: true,
+            freeze_message: __('Loading Your Catering Site... Please Wait'),
+            callback: (r) => {
+                if(r.message){
+                    frm.set_value('site_name', r.message);
+                frm.set_df_property('site_name', 'read_only', true);
+                }
+            }
+        })
+	},
+});
